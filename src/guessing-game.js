ed9decf8e3987650a624b5cd0a62ade5f353bad8
k0nnte <bystrov.igor25@gmail.com>
2023-09-08 10:38:07 +0300

    complite

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.mas = [];
+    }
 
     setRange(min, max) {
-
+        this.mas = [];
+        for(let i = min; i<=max;i++){
+            this.mas.push(i);
+        }
     }
 
     guess() {
-
+        return this.mas[Math.floor(this.mas.length/2)];
     }
 
     lower() {
-
+        this.setRange(this.mas[0],this.mas[Math.floor(this.mas.length/2)]);
     }
 
     greater() {
-
+        this.setRange(this.mas[Math.floor(this.mas.length/2)],this.mas[this.mas.length-1]);
     }
 }
 
